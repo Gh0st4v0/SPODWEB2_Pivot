e531475d37e4fc16e469fcb5923e1654b3f8e65d
MurilloAssis <murillo.assis@kiman.com.br>
2025-11-18 20:42:13 -0300

    Alter project

---
+++ src/components/Header/header.jsx
@@ -0,0 +1,25 @@
+import '../../assets/css/style.css'
+import '../../assets/css/consultasAdm.css'
+import '../../assets/css/login.css';
+import logo from "../../assets/img/logo_spmedgroup_v1 1.png"
+import userimg from "../../assets/img/user_img.png"
+import { parseJwt } from '../../services/auth';
+import { Link } from 'react-router-dom';
+
+export default function header (){
+        return (
+            <header>
+                <div className="container container_header">
+                    <img className="logo" src={logo} alt="Logo SP Medical" />
+                    <div className="space_left">
+                        <nav className="links_uteis">
+                            <Link to="/#">Inicio</Link>
+                            <Link to="/#">Consulta</Link>
+                            <Link to="/#">Equipe</Link>
+                            <Link to="/#">Contate-nos</Link>                        </nav>
+                        
+                    </div>
+                </div>
+            </header>
+        )
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import {
+  Route,
+  BrowserRouter as Router,
+  Redirect,
+  Switch,
+} from "react-router-dom";
+import { parseJwt, usuarioAutenticado } from "./services/auth";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import Login from "./pages/login/login";
+import Administrador from "./pages/Administrador/adm";
+import Medico from "./pages/medico/medico";
+import Consultas from "./pages/consultas";
+import Mapa from "./pages/mapa/mapa";
+import home from "./pages/home";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const routing = (
+  <Router>
+    <div>
+      <Switch>
+        <Route path="/login" component={Login} />
+        {/* <PermissaoAdm path="/listarConsultas" component={Administrador} />
+        <PermissaoAdm path="/mapa" component={Mapa} />
+        <PermissaoMedico path="/minhasConsultasMedico" component={Medico} /> */}
+        <Route path="/home" component={home} />
+
+        <Route path="/consultas" component={Consultas} />
+        <Route exact patch="/">
+          <Redirect to="/login" />
+        </Route>
+      </Switch>
+    </div>
+  </Router>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+ReactDOM.render(routing, document.getElementById("root"));
+
 reportWebVitals();
