e7186815dd27d7311e8bcbf66b440d6b6dc339a6
Gh0st4v0 <89959988+Gh0st4v0@users.noreply.github.com>
2025-11-25 21:50:16 -0300

    Add files via upload

---
+++ home.jsx
@@ -0,0 +1,44 @@
+import React, { useState, Fragment } from "react";
+import styles from "./home.module.css";
+
+import Header from "../components/Header/header";
+import Footer from "../components/Footer/footer";
+import medicosJson from "../data/medicos.json";
+import pacientesJson from "../data/pacientes.json";
+import consultasJson from "../data/consultas.json";
+
+const Home = () => {
+  const [medicos, setMedicos] = useState(medicosJson);
+  const [pacientes, setPacientes] = useState(pacientesJson);
+  const [consultas, setConsultas] = useState(consultasJson);
+
+  const totalMedicos = medicos?.length;
+  const totalPacientes = pacientes?.length;
+  const consultasAgendadas = consultas?.filter(c => c.status === 'Agendada').length;
+
+  return (
+    <Fragment>
+      <Header/>
+      <div className={styles.container}>
+        <h2 className={styles.title}>✨ Visão Geral do Sistema</h2>
+
+        <div className={styles.statsContainer}>
+          <div className={styles.statItem}>
+            Total de Médicos: <strong>{totalMedicos}</strong>
+          </div>
+          <div className={styles.statItem}>
+            Total de Pacientes: <strong>{totalPacientes}</strong>
+          </div>
+        </div>
+        <div className={styles.statsContainer}>
+          <div className={styles.statItem}>
+            Consultas Agendadas: <strong>{consultasAgendadas}</strong>
+          </div>
+        </div>
+      </div>
+      <Footer/>
+    </Fragment>
+  );
+};
+
+export default Home;
